67fc76c359c9e1e17506506a4b387c4736360d22
gabriel-ribeiro_valtech <gabriel.ribeiro@valtech.com>
2022-11-18 11:32:07 -0300

    Starting project with chakra UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,32 @@
-import { demos } from '@/lib/demos';
-import Link from 'next/link';
+'use client';
+
+import '../src/styles/global.css';
+import { ChakraProvider, extendTheme, Flex, Text } from '@chakra-ui/react';
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+};
+
+const theme = extendTheme({ colors });
 
 export default function Page() {
   return (
-    <div className="space-y-6">
-      <div className="space-y-8 text-white">
-        {demos
-          .filter((section) =>
-            section.items.some((x) => typeof x.isDisabled === 'undefined'),
-          )
-          .map((section) => {
-            return (
-              <div key={section.name} className="space-y-3">
-                <div className="text-xs font-semibold uppercase tracking-wider text-zinc-500">
-                  {section.name}
-                </div>
-
-                <div className="grid grid-cols-2 gap-5">
-                  {section.items
-                    .filter((item) => !item.isDisabled)
-                    .map((item) => {
-                      return (
-                        <Link
-                          href={`/${item.slug}`}
-                          key={item.name}
-                          className="block space-y-1.5 rounded-lg border border-white/10 px-4 py-3 hover:border-white/20"
-                        >
-                          <div>{item.name}</div>
-
-                          {item.description ? (
-                            <div className="line-clamp-3 text-sm text-zinc-400">
-                              {item.description}
-                            </div>
-                          ) : null}
-                        </Link>
-                      );
-                    })}
-                </div>
-              </div>
-            );
-          })}
-      </div>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Flex direction="column">
+        <Text color={'black'} fontWeight="bold" fontSize="2xl">
+          Hello World
+        </Text>
+        <Text color={'red'} fontWeight="bold" fontSize="2xl">
+          Hello World
+        </Text>
+        <Text color={'cyan'} fontWeight="bold" fontSize="2xl">
+          Hello World
+        </Text>
+      </Flex>
+    </ChakraProvider>
   );
 }
